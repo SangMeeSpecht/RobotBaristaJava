264a91f23b76370d9b88b2d1ff5a754b8cd50f07
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-08-18 00:37:23 -0500

    Fix indentation

--- src/model/Ingredient.java
+++ src/model/Ingredient.java
@@ -2,15 +2,15 @@ package model;
 
 public class Ingredient {
     public String ingredient;
-	public Integer count;
-	private static final Integer COUNT_MAX = 10;
-	Float price;
+    public Integer count;
+    private static final Integer COUNT_MAX = 10;
+    Float price;
   	
-	public Ingredient(String ingredient, Integer count, Float price) {
-	    this.ingredient = ingredient;
-		this.count = count;
-		this.price = price;
-	}
+    public Ingredient(String ingredient, Integer count, Float price) {
+    	this.ingredient = ingredient;
+    	this.count = count;
+    	this.price = price;
+    }
 	
     public void useIngredient(Integer subtrahend){
         count -= subtrahend;
